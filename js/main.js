ff201c6aaf5604a743b95cf362f34557134966ba
lemoniks <bohdan.luts22@gmail.com>
2020-10-28 21:24:18 +0200

    authorized in js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,82 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+// day 1
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInform = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('user');
+
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+  loginInput.style.borderColor = '';
+}
+
+
+function authorized() {
+  console.log('authorized');
+  function logOut() {
+    login = '';
+    localStorage.removeItem('user');
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+
+  }
+
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+  buttonOut.addEventListener('click', logOut);
+}
+function notAuthorized() {
+  console.log('NOT authorized');
+
+  function logIn(event) {
+    event.preventDefault();
+    if (loginInput.value.trim()) {
+      login = loginInput.value;
+      localStorage.setItem('user', login);
+      toggleModalAuth();
+      buttonAuth.removeEventListener('click', toggleModalAuth);
+      closeAuth.removeEventListener('click', toggleModalAuth);
+      logInform.removeEventListener('submit', logIn);
+      logInform.reset();
+      checkAuth();
+    } else {
+      alert('need password :)');
+      loginInput.style.borderColor = 'red';
+    }
+  }
+
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInform.addEventListener('submit', logIn);
+  modalAuth.addEventListener('click', function(event) {
+    if (event.target.classList.contains('is-open')) {
+      toggleModalAuth()
+    }
+  })
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized()
+  }
+}
+
+checkAuth();
